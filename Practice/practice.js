d858a09d139e241dbcf585a951df4954db27dc6f
DSunilDev <sunilkumardeveops@gmail.com>
2023-06-05 08:04:45 +0530

    Request url and Repsonse

--- Practice/practice.js
+++ Practice/practice.js
@@ -2,6 +2,24 @@ const http=require('http');
 
 function handle(request,response)
 {
+    if(request.url==='/sunil')
+    {
+        response.statusCode=200;
+        response.end('<h1>Welcome Sunil</h1>'+new Date().toISOString())
+
+    }
+    else if(request.url=='/Fren')
+    {
+        response.statusCode=200;
+        response.end('<h1> Fren ,Welcome <br> Time is </h1>'+ new Date().toISOString() )
+    }
+    else
+    {
+        response.statusCode=200;
+        response.end('<h1>Anonymous User found</h1>')
+    }
+
+
     response.statusCode=200;
     response.end('<marquee> Hey </marquee>')
 }
--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-let username="Manu";
+const http=require('http');
 
-console.log(username);
+function handlerequest(request,response)
+{
+    if(request.url==='/currenttime')
+    {
+        response.statusCode=200;
+        response.end('<h1>'+ new Date().toISOString() +'</h1>');
+    }
+    else if(request.url==='/')
+    {
+        response.statusCode=200;
+        response.end('<h1>HEllo Worls</h1>')
+    }
+
+    response.statusCode=200;
+    response.end('<h1>Hello World</h1>')
+
+}
+
+
+const server=http.createServer(handlerequest)
+
+
+
+server.listen(3000);
+
+//Custom NodeJs Server and Simple handle request..
