09134bdb52b01f78f42e1722e440aa3ce6708857
DSunilDev <sunilkumardeveops@gmail.com>
2023-06-05 16:33:23 +0530

    Working with EJS

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
+//Ejs TEmplate adding
+
+
 const express=require('express')
+const fs=require('fs')
 const path=require('path')
 
 const app=express();
 
 app.use(express.static('styles'));
 
+app.use(express.urlencoded({extended:false}))
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 app.get('/',function(req,res)
 {
-    res.send('<h1>HEllo</h1>')
+    const filepath=path.join(__dirname,'views','members.json')
+    const filedata=fs.readFileSync(filepath)
+    const memberdata=JSON.parse(filedata)
+    res.render('index',{ numberofMembers:memberdata.length,members:memberdata});
 })
 
 app.get('/Home',function(req,res)
 {
-    const htmlfile=path.join(__dirname,'views','index.html')
-    res.sendFile(htmlfile);
+    res.render('index')
 })
+
+app.get('/data',function(req,res){
+    const filepath=path.join(__dirname,'views','members.json')
+    const filedata=fs.readFileSync(filepath)
+    const memberdata=JSON.parse(filedata)
+    res.send(memberdata);
+
+})
+
+
+app.post('/Home',function(req,res)
+{
+    const membername=req.body;
+    const filepath=path.join(__dirname,'views','members.json')
+    const filedata=fs.readFileSync(filepath)
+    const memberdata=JSON.parse(filedata)
+    memberdata.push(membername);
+    fs.writeFileSync(filepath,JSON.stringify(memberdata));
+    res.send('<h1>done</h1>')
+    
+})
+
+
 
 app.get('/Nature',function(req,res)
 {
-    const htmlfile=path.join(__dirname,'views','nature.html')
-    res.sendFile(htmlfile);
+    res.render('nature')
 })
 
 app.get('/devs',function(req,res)
 {
-    const htmlfile=path.join(__dirname,'views','devs.html')
-    res.sendFile(htmlfile);
+    res.render('devs')
 })
 
 app.listen(3000);
